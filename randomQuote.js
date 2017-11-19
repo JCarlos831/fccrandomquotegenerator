1d3d70858e285d97701159806c1c46de954866b9
Juan Montoya <jcmontoya@gmail.com>
2017-11-19 22:48:29 +0000

    add hip hop quotes

--- randomQuote.js
+++ randomQuote.js
@@ -1,14 +1,19 @@
 /*global $*/
 $(document).ready(function(){
+    
     function newQuote(){
         
-        var quotes = ["Hello, World!", "Hello, Sea!", "Hello, Sky!"];
-        var author = ["-Author1", "-Author2", "-Author3"];
+        var quotes = ["No matter how hard it gets, stick your chest out, keep your head up and handle it.", "Everyday women and men become legends.", "Being happy is the goal, but greatness is my mission.", "Reach for the stars so if you fall you land on the clouds"];
+        var author = ["-Tupac Shakur", "-Common", "-Childish Gambino", "-Kanye West"];
         
         var randomNumber = Math.floor((Math.random()*quotes.length));
         var randomQuote = quotes[randomNumber];
         var randomAuthor = author[randomNumber];
         
+        $(".quote").text(randomQuote);
+        $(".author").text(randomAuthor);
     }
-    
+        $("#quoteButton").on("click", function(){
+            newQuote();
+        });
 });
