6399befba6656e7e546828a159db65ac948a0a1d
Juan Montoya <jcmontoya@gmail.com>
2017-11-20 06:28:28 +0000

    add Nas quotes to hiphopQuotes array

--- randomQuote.js
+++ randomQuote.js
@@ -8,8 +8,8 @@ $(document).ready(function(){
     
     function newlyric(){
         
-        var hiphopQuotes = ["I could let these dream killers kill my self-esteem, or use my arrogance as a steam to power my dreams.","No matter how hard it gets, stick your chest out, keep your head up and handle it.", "Everyday women and men become legends.", "Being happy is the goal, but greatness is my mission.", "Reach for the stars so if you fall you land on the clouds"];
-        rapper = ["- Kanye West, 'Last Call'","- Tupac Shakur, 'Me Against the World'", "- Common, 'Glory'", "- Childish Gambino, 'I'm Alright'", "- Kanye West, 'Homecoming'"];
+        var hiphopQuotes = ["No idea is original, there's nothing new under the sun, it's never what you do, but how it's done","Much success to you, even if you wish me the opposite","Turnin' nothin' into somethin' is God work and you get nothin' without struggle and hard work","I won't say I'm the baddest, or portray that role, but I'm in the top two, and father's gettin' old","My mama ain't raise no fool because my mama ain't raise me, fool","Climb the ladder to success, escalator style","I'm trying to right my wrongs, but it's funny, them same wrongs helped me write this song","I could let these dream killers kill my self-esteem, or use my arrogance as a steam to power my dreams","No matter how hard it gets, stick your chest out, keep your head up and handle it", "Everyday women and men become legends", "Being happy is the goal, but greatness is my mission", "Reach for the stars so if you fall you land on the clouds"];
+        rapper = ["- Nas, 'No idea's original","- Nas, 'Nas is Like'","- Nas, 'Doo Rags'","- Big Daddy Kane, 'Cause I Can Do It Right'","- Sean Price, '60 Bar Dash'", "- The Notorious B.I.G., 'You're Noboby'","- Kanye West, 'Touch the Sky'","- Kanye West, 'Last Call'","- Tupac Shakur, 'Me Against the World'", "- Common, 'Glory'", "- Childish Gambino, 'I'm Alright'", "- Kanye West, 'Homecoming'"];
         
             randomNumber = Math.floor(Math.random()*hiphopQuotes.length);
             randomQuote = hiphopQuotes[randomNumber];
